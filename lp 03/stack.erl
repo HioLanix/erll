73dcd3c0e8a3c85e90e72e09a1216f187719629c
HioLanix <122531348+HioLanix@users.noreply.github.com>
2024-11-25 23:04:49 +0700

    Delete lp 03 directory

--- lp 03/stack.erl
+++
@@ -1,8 +0,0 @@
--module(cus_queue).
--export([new_queue/0, enqueue, dequeue/1]).
-
--type stack() :: empty | [{stack, integer{}}].
--type data() :: stack() | {error,empty} | some_error.
--spec new_stack() ->
-    [{stack, Elem}];
-push(Elem) 
--- lp 03/testmodule.erl
+++
@@ -1,5 +0,0 @@
--module(testmodule).
-
--export([add/2, subtr/2, mult/2, divis/2]).
-
-add(X, Y) -> X + Y.
